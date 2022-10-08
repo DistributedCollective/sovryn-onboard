5bd9d5b7cedd6c1f5c433b9a0de282661287cb00
Victor Creed <victor@sovryn.app>
2022-10-08 12:52:23 +0300

    fix: can only disconnect from single wallet by label

--- packages/core/src/disconnect.ts
+++ packages/core/src/disconnect.ts
@@ -3,9 +3,17 @@ import { removeWallet } from "./store/actions";
 import { disconnectWallet$ } from "./streams";
 import type { WalletState } from "./types";
 
-async function disconnect(module: string): Promise<WalletState[]> {
-  disconnectWallet$.next(module);
-  removeWallet(module);
+async function disconnect(module?: string): Promise<WalletState[]> {
+  if (module) {
+    disconnectWallet$.next(module);
+    removeWallet(module);
+  } else {
+    const { wallets } = state.get();
+    wallets.forEach(({ label }) => {
+      disconnectWallet$.next(label);
+      removeWallet(label);
+    });
+  }
 
   return state.get().wallets;
 }
