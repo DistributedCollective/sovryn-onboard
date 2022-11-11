f0715ab1b875e97b201125c4168b57a11b82a49e
Victor Creed <victor@sovryn.app>
2022-11-11 09:55:14 +0200

    fix: update dialog styles

--- packages/onboard-react/src/components/HardwareWallets/HardwareWallets.tsx
+++ packages/onboard-react/src/components/HardwareWallets/HardwareWallets.tsx
@@ -31,7 +31,28 @@ export const HardwareWallets: FC = () => {
       ) : (
         <div>
           <button onClick={handleStepBack} className={styles.closeButton}>
-            <Icon icon={IconNames.ARROW_BACK} />
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clipPath="url(#clip0_2955_2007)">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M2.35588 10.4992L24 10.4992V13.4992L2.35588 13.4992V10.4992Z"
+                  fill="currentColor"
+                />
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12.1173 0.116455L14.2386 2.23778L4.4772 11.9992L14.2386 21.7606L12.1173 23.8819L0.234558 11.9992L12.1173 0.116455Z"
+                  fill="currentColor"
+                />
+              </g>
+            </svg>
           </button>
           <div>
             <HardwareWalletSteps step={step} onStepChanged={setStep} />
